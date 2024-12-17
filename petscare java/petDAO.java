190827cef6c08ece943363d3f46d414aafd47c05
mridul294 <135833811+mridul294@users.noreply.github.com>
2024-12-17 13:02:35 +0530

    Delete petscare java directory

--- petscare java/petDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
- interface PetDAO {
-    void addPet(Pet pet);
-    Pet getPetById(int id);
-    List<Pet> getAllPets();
-    void updatePet(Pet pet);
-    void deletePet(int id);
-}
